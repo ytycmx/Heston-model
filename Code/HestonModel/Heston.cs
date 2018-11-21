69b9460ac29f72c4123c755433817943cd0998b9
Witold <gawlikowicz@gmail.com>
2018-11-21 00:17:41 +0000

    Initial commit

---
+++ Code/HestonModel/Heston.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using HestonModel.Interfaces;
+
+namespace HestonModel
+{
+
+    /// <summary>
+    /// This class will be used for grading. Please keep it within "HestonModel" namespace, 
+    /// Don't remove any of the methods and don't modify their signatures.
+    /// Your code should be implemented in other classes (or even projects if you wish), and the relevant functionality should only be called here and outputs returned.
+    /// </summary>
+    public static class Heston
+    {
+        /// <summary>
+        /// Method for calibrating the heston model parameters.
+        /// </summary>
+        /// <param name="guessModelParameters">Object implementing IHestonModelParameters interface containing the risk-free rate, initial stock price
+        /// and initial guess parameters to be used in calibration.</param>
+        /// <param name="referenceData">A colection of objects implementing IOptionMarketData<IEuropeanOption> interface. 
+        /// These should contain the reference data used for calibration.</param>
+        /// <param name="calibrationSettings">An object implementing ICalibrationSettings interface.</param>
+        /// <returns>Object implementing IHestonCalibrationResult interface which contains calibrated model parameters and addtional diagnostic information</returns>
+        public static IHestonCalibrationResult CalibrateHestonParameters(IHestonModelParameters guessModelParameters, IEnumerable<IOptionMarketData<IEuropeanOption>> referenceData, ICalibrationSettings calibrationSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Price a call or put European option in the Heston model using the 
+        /// Heston formula. This should be accurate to 5 decimal places
+        /// </summary>
+        /// <param name="parameters">Object implementing IHestonModelParameters interface.</param>
+        /// <param name="europeanOption">Object implementing IEuropeanOption interface, containing the option parameters.</param>
+        /// <returns>Option price</returns>
+        public static double HestonEuropeanOptionPrice(IHestonModelParameters parameters, IEuropeanOption europeanOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Price a call or put European option in the Heston model using the 
+        /// Monte-Carlo method. Accuracy will depend on number of time steps and samples
+        /// </summary>
+        /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
+        /// <param name="europeanOption">Object implementing IEuropeanOption interface, containing the option parameters.</param>
+        /// <param name="monteCarloSimulationSettings">An object implementing IMonteCarloSettings object and containing simulation settings.</param>
+        /// <returns>Option price</returns>
+        public static double HestonEuropeanOptionPriceMC(IHestonModelParameters parameters, IEuropeanOption europeanOption, IMonteCarloSettings monteCarloSimulationSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Price a call or put Asian option in the Heston model using the 
+        /// Monte-Carlo method. Accuracy will depend on number of time steps and samples</summary>
+        /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
+        /// <param name="asianOption">Object implementing IAsian interface, containing the option parameters.</param>
+        /// <param name="monteCarloSimulationSettings">An object implementing IMonteCarloSettings object and containing simulation settings.</param>
+        /// <returns>Option price</returns>
+        public static double HestonAsianOptionPriceMC(IHestonModelParameters parameters, IAsianOption asianOption, IMonteCarloSettings monteCarloSimulationSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Price a lookback option in the Heston model using the  
+        /// a Monte-Carlo method. Accuracy will depend on number of time steps and samples </summary>
+        /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
+        /// <param name="maturity">An object implementing IOption interface and containing option's maturity</param>
+        /// <param name="monteCarloSimulationSettings">An object implementing IMonteCarloSettings object and containing simulation settings.</param>
+        /// <returns>Option price</returns>
+        public static double HestonLookbackOptionPriceMC(IHestonModelParameters parameters, IOption maturity, IMonteCarloSettings monteCarloSimulationSettings)
+        {
+            throw new NotImplementedException();
+        }       
+    }
+}
