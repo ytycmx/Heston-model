83aa3d403d62b900a82f5948ba1f4a243d1d0e93
Witold <gawlikowicz@gmail.com>
2018-11-21 11:52:43 +0000

    Modified xml descriptions

--- Code/HestonModel.Tests/Tests.cs
+++ Code/HestonModel.Tests/Tests.cs
@@ -9,5 +9,6 @@ namespace HestonModel.Tests
     public class Tests
     {   
         //Tests will be added here after the deadline.
+        //If you wish to add your own tests, then please add a new class or project.
     }
 }
--- Code/HestonModel/Heston.cs
+++ Code/HestonModel/Heston.cs
@@ -5,32 +5,31 @@ using HestonModel.Interfaces;
 namespace HestonModel
 {
 
-    /// <summary>
-    /// This class will be used for grading. Please keep it within "HestonModel" namespace, 
-    /// Don't remove any of the methods and don't modify their signatures.
+    /// <summary> 
+    /// This class will be used for grading. 
+    /// Don't remove any of the methods and don't modify their signatures. Don't change the namespace. 
     /// Your code should be implemented in other classes (or even projects if you wish), and the relevant functionality should only be called here and outputs returned.
+    /// You don't need to implement the interfaces that have been provided if you don't want to.
     /// </summary>
     public static class Heston
     {
         /// <summary>
-        /// Method for calibrating the heston model parameters.
+        /// Method for calibrating the heston model.
         /// </summary>
         /// <param name="guessModelParameters">Object implementing IHestonModelParameters interface containing the risk-free rate, initial stock price
-        /// and initial guess parameters to be used in calibration.</param>
-        /// <param name="referenceData">A colection of objects implementing IOptionMarketData<IEuropeanOption> interface. 
-        /// These should contain the reference data used for calibration.</param>
+        /// and initial guess parameters to be used in the calibration.</param>
+        /// <param name="referenceData">A collection of objects implementing IOptionMarketData<IEuropeanOption> interface. These should contain the reference data used for calibration.</param>
         /// <param name="calibrationSettings">An object implementing ICalibrationSettings interface.</param>
-        /// <returns>Object implementing IHestonCalibrationResult interface which contains calibrated model parameters and addtional diagnostic information</returns>
+        /// <returns>Object implementing IHestonCalibrationResult interface which contains calibrated model parameters and additional diagnostic information</returns>
         public static IHestonCalibrationResult CalibrateHestonParameters(IHestonModelParameters guessModelParameters, IEnumerable<IOptionMarketData<IEuropeanOption>> referenceData, ICalibrationSettings calibrationSettings)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// Price a call or put European option in the Heston model using the 
-        /// Heston formula. This should be accurate to 5 decimal places
+        /// Price a European option in the Heston model using the Heston formula. This should be accurate to 5 decimal places
         /// </summary>
-        /// <param name="parameters">Object implementing IHestonModelParameters interface.</param>
+        /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
         /// <param name="europeanOption">Object implementing IEuropeanOption interface, containing the option parameters.</param>
         /// <returns>Option price</returns>
         public static double HestonEuropeanOptionPrice(IHestonModelParameters parameters, IEuropeanOption europeanOption)
@@ -39,8 +38,7 @@ namespace HestonModel
         }
 
         /// <summary>
-        /// Price a call or put European option in the Heston model using the 
-        /// Monte-Carlo method. Accuracy will depend on number of time steps and samples
+        /// Price a European option in the Heston model using the Monte-Carlo method. Accuracy will depend on number of time steps and samples
         /// </summary>
         /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
         /// <param name="europeanOption">Object implementing IEuropeanOption interface, containing the option parameters.</param>
@@ -52,7 +50,7 @@ namespace HestonModel
         }
 
         /// <summary>
-        /// Price a call or put Asian option in the Heston model using the 
+        /// Price a Asian option in the Heston model using the 
         /// Monte-Carlo method. Accuracy will depend on number of time steps and samples</summary>
         /// <param name="parameters">Object implementing IHestonModelParameters interface, containing model parameters.</param>
         /// <param name="asianOption">Object implementing IAsian interface, containing the option parameters.</param>
